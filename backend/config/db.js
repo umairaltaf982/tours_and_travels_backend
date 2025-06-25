7287fea24d8bc813b1efaeadcdd90fdb2c7f8bae
Muhammad Umair Altaf <mumair.altaf@systemsltd.com>
2025-06-25 02:59:02 -0700

    added other files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
+require('dotenv').config(); // Ensure .env is loaded before using process.env
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI not set in environment variables');
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
