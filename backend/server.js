05ebe6232ef2008bf05097922860ca9a96c45fb7
Umair Altaf <umairaltaf982@gmail.com>
2025-06-26 12:08:37 +0500

    added many files

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
